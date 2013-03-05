6c88398f9e19349e850961d247eab7d24af7ee9c
gongzhitaao <zhitaao.gong@gmail.com>
2013-03-04 22:45:19 -0600

    1. add polygon schema for AsmXml parsing, but not tested yet 2. change chunkSize to 32M

--- SigSpatial2013/main.cpp
+++ SigSpatial2013/main.cpp
@@ -68,18 +68,51 @@ int main(int argc, char *argv[])
   <document name=\"gml:Point\">\
     <attribute name=\"srsName\" ignore=\"yes\"/>\
     <attribute name=\"xmlns:gml\" ignore=\"yes\"/>\
-    <collection name=\"gml:coordinates\" type=\"text\">\
+    <element name=\"gml:coordinates\" type=\"text\">\
       <attribute name=\"decimal\" ignore=\"yes\"/>\
       <attribute name=\"cs\" ignore=\"yes\"/>\
       <attribute name=\"ts\" ignore=\"yes\"/>\
-    </collection>\
+    </element>\
+  </document>\
+</schema>";
+
+    const char polygonSchema[] = "\
+<schema>\
+  <document name=\"gml:Polygon\">\
+    <attribute name=\"srsName\" ignore=\"yes\"/>\
+    <attribute name=\"xmlns:gml\" ignore=\"yes\"/>\
+    <element name=\"gml:outerBoundaryIs\">\
+      <element name=\"gml:LinearRing\">\
+        <element name=\"gml:coordinates\" type=\"text\">\
+          <attribute name=\"decimal\" ignore=\"yes\"/>\
+          <attribute name=\"cs\" ignore=\"yes\"/>\
+          <attribute name=\"ts\" ignore=\"yes\"/>\
+        </element>\
+      </element>\
+    </element>\
+    <element name=\"gml:innerBoundaryIs\">\
+      <collection name=\"gml:LinearRing\">\
+        <element name=\"gml:coordinates\" type=\"text\">\
+          <attribute name=\"decimal\" ignore=\"yes\"/>\
+          <attribute name=\"cs\" ignore=\"yes\"/>\
+          <attribute name=\"ts\" ignore=\"yes\"/>\
+        </element>\
+      </collection>\
+    </element>\
   </document>\
 </schema>";
 
     AXParseContext context;
-    unsigned chunkSize = 256;
+    /* 256 byte chunkSize is enough for point parsing.
+     * But as points in a polygon may be as many as 200k, the estimated
+     * chunkSize for the raw data may be around 16M.  So set the chunkSize
+     * to 32M for safety.
+     */
+    unsigned chunkSize = 32 * 1024 * 1024;
 
     AXElement *gmlpoint;
+    AXElement *gmlpoly, *ring;
+    AXElement *coord;
 
     ax_initialize(malloc, free);
 
@@ -94,7 +127,8 @@ int main(int argc, char *argv[])
         fin_point >> tmppt.id >> ch >> tmppt.seq >> ch;
         fin_point.getline(xmlstr, 256);
         gmlpoint = ax_parse(&context, xmlstr, pointClass, 1);
-        fout << asString(&gmlpoint->firstChild->attributes[0]) << endl;
+        coord = ax_getElement(gmlpoint, 0);
+        fout << asString(&coord->attributes[0]) << endl;
     }
     fin_point.close();
     fout.close();
